83de05b0781969ea8658901dad7716268ecb0404
kennethhdc <kennethhdc@gmail.com>
2024-10-03 16:49:32 +0000

    Update

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:sixam_mart/util/app_constants.dart';
 
-ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
+ThemeData dark({Color color = const Color(0xFFda291c)}) => ThemeData(
   fontFamily: AppConstants.fontFamily,
   primaryColor: color,
-  secondaryHeaderColor: const Color(0xFF009f67),
+  secondaryHeaderColor: Color.fromARGB(255, 159, 45, 0),
   disabledColor: const Color(0xffa2a7ad),
   brightness: Brightness.dark,
   hintColor: const Color(0xFFbebebe),
   cardColor: const Color(0xFF30313C),
   shadowColor: Colors.white.withOpacity(0.03),
-  textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white70)),
   textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: color)),
   colorScheme: ColorScheme.dark(primary: color, secondary: color).copyWith(surface: const Color(0xFF191A26)).copyWith(error: const Color(0xFFdd3135)),
   popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF29292D), surfaceTintColor: Color(0xFF29292D)),
--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -13,7 +13,7 @@ class AppConstants {
   static const String webHostedUrl = 'https://kimii.app';
   static const bool useReactWebsite = false;
 
-   static const String webHostedUrl = 'https://kimii.app';
+   static const String baseUrl  = 'https://kimii.app';
   static const String categoryUri = '/api/v1/categories';
   static const String bannerUri = '/api/v1/banners';
   static const String storeItemUri = '/api/v1/items/latest';
